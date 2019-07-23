0a6a1e24037ec73cd01776de480945a69aee3669
Jan Folfas <folfix@gmail.com>
2019-07-23 20:14:17 +0200

    Initial commit, added maven-java8-spring template

---
+++ maven-java8-spring/src/main/java/com/examplero/gruppo/exampleroartifacto/ExampleroArtifactoApplication.java
@@ -0,0 +1,13 @@
+package com.examplero.gruppo.exampleroartifacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleroArtifactoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleroArtifactoApplication.class, args);
+    }
+
+}
